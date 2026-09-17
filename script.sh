cfbccbf8419d6e66832f994cdb657e28a1b39efc
Sara Isabel <a01752599@tec.mx>
2026-09-17 12:08:49 -0600

    Añade validación de argumentos

--- script.sh
+++ script.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
+
+# Validar que se hayan recibido exactamente 2 argumentos
 if [ "$#" -ne 2 ]; then
-    echo "Uso: ./script.sh \"palabra_o_frase\" \"directorio\""
+    echo "Uso: $0 <palabra_o_frase> <directorio>"
     exit 1
 fi
+
+# Validar que la ruta exista y sea un directorio
 if [ ! -d "$2" ]; then
-    echo "Error: la ruta proporcionada no existe o no es un directorio."
+    echo "Error: '$2' no existe o no es un directorio válido."
     exit 1
 fi
