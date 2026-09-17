0a5231a6e3cadbe2ff7f567f90676887b024f168
Sara Isabel <a01752599@tec.mx>
2026-09-17 11:44:53 -0600

    Añade validación de argumentos

--- script.sh
+++ script.sh
@@ -1,21 +1,9 @@
-# ==========================================
-# PARTE 1: Validación de Argumentos
-# ==========================================
-
-# 1. Verificar si el número de parámetros ($#) es diferente (-ne) de 2
+#!/bin/bash
 if [ "$#" -ne 2 ]; then
-    echo "Error: Parámetros incorrectos."
-    echo "Uso: $0 <palabra_a_buscar> <ruta_del_directorio>"
+    echo "Uso: ./script.sh \"palabra_o_frase\" \"directorio\""
     exit 1
 fi
-
-# Asignar los argumentos a variables legibles
-PALABRA="$1"
-DIRECTORIO="$2"
-
-# 2. Verificar si el camino proporcionado NO es un directorio válido (! -d)
-if [ ! -d "$DIRECTORIO" ]; then
-    echo "Error: El directorio '$DIRECTORIO' no existe o no es válido."
+if [ ! -d "$2" ]; then
+    echo "Error: la ruta proporcionada no existe o no es un directorio."
     exit 1
 fi
-
